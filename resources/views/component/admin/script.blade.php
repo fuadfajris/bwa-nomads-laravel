24179fa2e31948b8d09599253cc291ca25cab6e4
fuad fajri soba <fuadfajrisoba@gmail.com>
2023-09-09 11:44:16 +0700

    add template sb-admin2

---
+++ resources/views/component/admin/script.blade.php
@@ -0,0 +1,16 @@
+<!-- Bootstrap core JavaScript-->
+<script src="{{url('admin-test/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{url('admin-test/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+ <!-- Core plugin JavaScript-->
+<script src="{{url('admin-test/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{url('admin-test/js/sb-admin-2.min.js')}}"></script>
+
+<!-- Page level plugins -->
+<script src="{{url('admin-test/vendor/chart.js/Chart.min.js')}}"></script>
+
+<!-- Page level custom scripts -->
+<script src="{{url('admin-test/js/demo/chart-area-demo.js')}}"></script>
+<script src="{{url('admin-test/js/demo/chart-pie-demo.js')}}"></script>
