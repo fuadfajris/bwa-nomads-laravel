9b446f7a4f1b4d9b7bfe1f26eff93f16865fe123
Fuad.Soba <fuad.soba@astralife.co.id>
2023-09-15 16:30:49 +0700

    update login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
+            <!-- @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
@@ -29,7 +29,24 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif -->
+            @guest
+            <form class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+              <button class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" type="button"
+              onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+                Masuk
+              </button>
+            </form>
+            @endguest
+
+            @auth
+            <form class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10" action="{{ url('logout') }}" method="POST">
+              @csrf
+              <button class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" type="submit">
+                Keluar
+              </button>
+            </form>
+            @endauth
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->namespace('Admin') ->group(function () {
+Route::prefix('admin')
+->middleware(['auth', 'admin'])
+->group(function(){
     Route::get('/',  [DashboardController::class, 'index']) ->name('dashboard');
     Route::get('/galleries',  [GalleriesController::class, 'index']) -> name('galleries');
     Route::get('/travel-package',  [TravelPackageController::class, 'index']) -> name('travel-package');
     Route::get('/transactions',  [TransactionsController::class, 'index']) -> name('transactions');
+
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__.'/auth.php';
